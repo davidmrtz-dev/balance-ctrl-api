016ff8af058de2be42b6c2c5a94ab10713512830
David Mtz <davidelimg1@gmail.com>
2023-01-29 21:59:11 -0600

    [WIP]: Updated cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-
-    resource "*",
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    resource '*',
+          headers: :any,
+          expose: ['access-token', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
   end
 end
