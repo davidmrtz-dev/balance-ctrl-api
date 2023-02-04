1d19c388b72afb840abcf4a6b43a11c0567291a5
David Mtz <davidelimg1@gmail.com>
2023-02-04 17:44:30 -0600

    [FEAT]: Implemented active factory.

---
+++ spec/factories/finance_active_factory.rb
@@ -0,0 +1,16 @@
+class FinanceActiveFactory
+  def self.described_class
+    FinanceActive
+  end
+
+  private
+
+  def options(params)
+    {
+      balance: params.fetch(:balance, nil),
+      income_frequency: params[:income_frequency] || :monthly,
+      income_date: params.fetch(:income_date, Date.today.at_beginning_of_month),
+      amount: params.fetch(:amount, 50_000.00)
+    }
+  end
+end
