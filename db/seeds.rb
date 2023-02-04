3d5ac18481afe3ebe53ff297106a78f439b63ea1
David Mtz <davidelimg1@gmail.com>
2023-02-03 20:28:53 -0600

    [CHORE]: Implemented seeds to retrieve data on endpoints.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,26 @@
-# return unless Rails.env.development? || Rails.env.staging?
+return unless Rails.env.development? || Rails.env.staging?
 
-# user = User.find_or_create_by!(
-#   email: 'user@example.com',
-#   password: 'password',
-#   password_confirmation: 'password'
-# )
+user = User.create!(
+  email: 'user@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
 
-# balance = Balance.find_or_create_by!(user: user)
+balance = Balance.find_or_create_by!(user: user)
 
-# 2.times.do |num|
-#   FinanceActive.create!(balance: balance)
-# end
+2.times do |num|
+  FinanceActive.create!(
+    balance: balance,
+    income_frequency: :monthly,
+    income_date: Date.today.at_beginning_of_month,
+    amount: 45000.00
+  )
+
+  FinanceObligation.create!(
+    balance: balance,
+    obligation_type: :fixed,
+    status: :active,
+    charge_date: Date.today.at_beginning_of_month,
+    amount: 4567.84
+  )
+end
