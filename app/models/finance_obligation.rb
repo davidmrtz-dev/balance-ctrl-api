bb63a60ebf0a68437eda57dad2217ca8bf3d81de
David Mtz <davidelimg1@gmail.com>
2023-02-03 20:07:13 -0600

    [FEAT]: Updated models with respective enums

--- app/models/finance_obligation.rb
+++ app/models/finance_obligation.rb
@@ -1,3 +1,6 @@
 class FinanceObligation < ApplicationRecord
   belongs_to :balance
+
+  enum obligation_type: { fixed: 0, current: 1 }
+  enum status: { active: 0, inactive: 1}
 end
--- spec/models/finance_obligation_spec.rb
+++ spec/models/finance_obligation_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe FinanceObligation, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:balance) }
+    it {  should define_enum_for(:obligation_type).with_values(%i[fixed current]) }
+    it {  should define_enum_for(:status).with_values(%i[active inactive]) }
   end
 end
