06b45e9a6d6d15749937e37d78094c651acdc35e
David Mtz <davidelimg1@gmail.com>
2023-02-04 17:49:03 -0600

    [FEAT]: Implemented passive factory.

---
+++ spec/factories/finance_obligation_factory.rb
@@ -0,0 +1,17 @@
+class FinanceObligationFactory
+  def self.described_class
+    FinanceObligation
+  end
+
+  private
+
+  def options(params)
+    {
+      balance: params.fetch(:balance, nil),
+      income_frequency: params[:obligation_type] || :fixed,
+      income_date: params[:status] || :active,
+      charge_date: params.fetch(:charge_date, Date.today),
+      amount: params.fetch(:amount, 3_500.85)
+    }
+  end
+end
