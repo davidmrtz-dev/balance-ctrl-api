cfcac7ac154187edfd9eddc65a44006f39d41d18
David Mtz <davidelimg1@gmail.com>
2023-02-04 22:17:00 -0600

    [FEAT]: Attached calculations to response.

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -2,4 +2,16 @@ class Balance < ApplicationRecord
   belongs_to :user
   has_many :finance_obligations, dependent: :destroy
   has_many :finance_actives, dependent: :destroy
+
+  def total_income
+    finance_actives.sum(:amount)
+  end
+
+  def total_expenses
+    finance_obligations.sum(:amount)
+  end
+
+  def total_balance
+    total_income - total_expenses
+  end
 end
