9e4dda9e72634d3e1a6300487e809d3097c04e53
David Mtz <davidelimg1@gmail.com>
2023-02-05 22:53:54 -0600

    [FEAT]: Updated controller tests

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::PaymentsController, type: :controller do
+  let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
+  let!(:balance) { BalanceFactory.create_with_attachments(user: user) }
+
   describe "GET /payments" do
     login_user
 
-    it "returns head ok" do
+    it "returns paginated fixed and current payments" do
       get :index
+
       expect(response).to have_http_status(:ok)
+      expect(parsed_response['fixed'].map { |o| o['id'] }).to match_array(FinanceObligation.fixed.ids)
+      expect(parsed_response['total_fixed']).to eq(4)
+      expect(parsed_response['current'].map { |o| o['id'] }).to match_array(FinanceObligation.current.ids)
+      expect(parsed_response['total_current']).to eq(2)
     end
   end
 end
