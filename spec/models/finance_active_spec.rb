505530a18112bf77aee53bc24ac5958ceaf09cc8
David Mtz <davidelimg1@gmail.com>
2023-02-10 21:42:29 -0600

    [CHORE]: Added test for outcome model

--- spec/models/finance_active_spec.rb
+++ spec/models/finance_active_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FinanceActive, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { is_expected.to belong_to(:balance) }
     it { should define_enum_for(:income_frequency).with_values(%i[weekly biweekly monthly]) }
     it { should define_enum_for(:active_type).with_values(%i[fixed current]) }
@@ -11,7 +11,7 @@ RSpec.describe FinanceActive, type: :model do
     let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
     let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
 
-    it 'should update current_amount from the balance' do
+    it 'should update balance current_amount attribute' do
       income = FinanceActiveFactory.create(balance: balance, amount: 5_000)
 
       expect(balance.current_amount.to_f).to eq 15_000
--- spec/models/finance_obligation_spec.rb
+++ spec/models/finance_obligation_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe FinanceObligation, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { is_expected.to belong_to(:balance) }
     it { should define_enum_for(:obligation_type).with_values(%i[fixed current]) }
+  end
+
+  describe '.update_current_balance' do
+    let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
+    let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
+
+    it 'should update balance current_amount attribute' do
+      outcome = FinanceObligationFactory.create(balance: balance, amount: 5_000)
+
+      expect(balance.current_amount).to eq 5_000
+    end
   end
 end
