0baddca87505aa55ffb7d056105b3d4c580f361c
David Mtz <davidelimg1@gmail.com>
2023-02-12 14:15:20 -0600

    [CHORE]: Added status column to payments

--- db/migrate/20230211210131_create_payments.rb
+++ db/migrate/20230211210131_create_payments.rb
@@ -3,6 +3,7 @@ class CreatePayments < ActiveRecord::Migration[6.1]
     create_table :payments do |t|
       t.references :paymentable, null: false, polymorphic: true, index: true
       t.decimal :amount, { precision: 8, scale: 2 }, default: '0.0', null: false
+      t.integer :status, null: false, default: :pending
       t.timestamps
     end
   end
