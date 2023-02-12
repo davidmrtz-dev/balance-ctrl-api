cb627bd2f0a9c69436b1c3b865feaf5cecbac397
David Mtz <davidelimg1@gmail.com>
2023-02-12 16:58:57 -0600

    [ENHANCE]: Outcome

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,6 @@ class Transaction < ApplicationRecord
 
   has_many :payments, as: :paymentable, dependent: :destroy
 
-  enum transaction_type: { current: 0, fixed: 1 }
+  enum transaction_type: { current: 0, fixed: 1 }, _default: :current
   enum frequency: { weekly: 0, biweekly: 1, monthly: 2 }
 end
--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Outcome, type: :model do
 
     describe 'when outcome transaction_type is :current' do
       it "should validate absence of 'quotas'" do
-        outcome = Outcome.new(balance: balance, transaction_type: :current, purchase_date: Time.zone.today, quotas: 12)
+        outcome = Outcome.new(balance: balance, purchase_date: Time.zone.today, quotas: 12)
         expect(outcome.valid?).to eq false
         expect(outcome.errors.full_messages.first).to eq("Quotas must be blank")
       end
@@ -33,7 +33,7 @@ RSpec.describe Outcome, type: :model do
   describe '#update_current_balance' do
     describe "when outcome is 'current'" do
       it 'should substract the amount from balance current_amount' do
-        Outcome.create(balance: balance, transaction_type: :current, amount: 5_000, purchase_date: DateTime.now)
+        Outcome.create(balance: balance, amount: 5_000, purchase_date: DateTime.now)
 
         expect(balance.reload.current_amount).to eq 5_000
       end
@@ -42,7 +42,7 @@ RSpec.describe Outcome, type: :model do
 
   describe '#generate_payment' do
     it "should create one payment for transaction_type 'current'" do
-      expect { Outcome.create(balance: balance, transaction_type: :current, amount: 5_000, purchase_date: DateTime.now) }
+      expect { Outcome.create(balance: balance, amount: 5_000, purchase_date: DateTime.now) }
         .to change { Payment.count }.by(1)
 
       expect(Payment.last.status).to eq 'applied'
