b3ab10f6e35a488c3f19d362bfdfb75cfb9b9175
David Mtz <davidelimg1@gmail.com>
2023-02-11 23:12:23 -0600

    [FIX]: Fixed issue sometimes tests failed

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :paymentable, polymorphic: true
 
-  # validate :one_payment_for_current_income, if: -> { paymentable.instance_of?(Income) }
-  # validate :one_payment_for_current_outcome, if: -> { paymentable.instance_of?(Outcome) }
   validate :one_payment_for_current_paymentable
 
   delegate :balance, to: :paymentable
@@ -20,18 +18,6 @@ class Payment < ApplicationRecord
       errors.add(:outcome, 'of type current can only have one payment')
     end
   end
-
-  # def one_payment_for_current_outcome
-  #   # byebug
-  #   errors.add(:outcome, 'of type current can only have one payment') if
-  #     paymentable.payments.size > 0 && paymentable.outcome_type.eql?('current')
-  # end
-
-  # def one_payment_for_current_income
-  #   # byebug
-  #   errors.add(:income, 'of type current can only have one payment') if
-  #     paymentable.payments.size > 0 && paymentable.income_type.eql?('current')
-  # end
 
   # def update_current_balance
   #   if paymentable.instance_of?(Outcome) &&
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe Payment, type: :model do
 
   describe 'validations' do
     describe 'one_payment_for_current_outcome' do
+      describe "when paymentable is Income and is 'fixed'" do
+        it 'should not be valid if payments > 0' do
+          income = Income.create(balance: balance, income_type: :current, income_frequency: :monthly)
+          payment_01 = Payment.create(paymentable: income)
+          payment_02 = Payment.new(paymentable: income)
+
+          expect(payment_02.valid?).to be_falsey
+          expect(payment_02.errors.full_messages.first).
+            to eq('Income of type current can only have one payment')
+        end
+      end
+
       describe "when paymentable is Outcome and is 'fixed'" do
         it 'should not be valid if payments > 0' do
           outcome = Outcome.create(balance: balance, outcome_type: :current, purchase_date: Time.zone.today)
@@ -20,20 +32,6 @@ RSpec.describe Payment, type: :model do
           expect(payment_02.errors.full_messages.first).
           to eq('Outcome of type current can only have one payment')
         end
-      end
-    end
-  end
-
-  describe 'one_payment_for_current_income' do
-    describe "when paymentable is Income and is 'fixed'" do
-      it 'should not be valid if payments > 0' do
-        income = Income.create(balance: balance, income_type: :current, income_frequency: :monthly)
-        payment_01 = Payment.create(paymentable: income)
-        payment_02 = Payment.new(paymentable: income)
-
-        expect(payment_02.valid?).to be_falsey
-        expect(payment_02.errors.full_messages.first).
-          to eq('Income of type current can only have one payment')
       end
     end
   end
