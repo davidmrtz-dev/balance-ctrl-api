16adc4531204eb049c73529cbcd96a1b8ee4b00e
David Mtz <davidelimg1@gmail.com>
2023-02-12 15:23:19 -0600

    [CHORE]: Added validations and tests for transactions types

--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -10,13 +10,26 @@ RSpec.describe Outcome, type: :model do
     it { should_not allow_value(:monthly).for(:frequency).on(:create) }
     it { should allow_value(Time.zone.today).for(:purchase_date).on(:create) }
 
-    # describe 'when paymentable transaction_type is :fixed' do
-    #   let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
-    #   let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
+    describe 'when outcome transaction_type is :fixed' do
+      let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
+      let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
 
-    #   it "should validate presence of 'quotas'" do
-    #     outcome = Outcome.new(balance: balance, )
-    #   end
-    # end
+      it "should validate presence of 'quotas'" do
+        outcome = Outcome.new(balance: balance, transaction_type: :fixed, purchase_date: Time.zone.today)
+        expect(outcome.valid?).to eq false
+        expect(outcome.errors.full_messages.first).to eq("Quotas can't be blank")
+      end
+    end
+
+    describe 'when outcome transaction_type is :current' do
+      let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
+      let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
+
+      it "should validate absence of 'quotas'" do
+        outcome = Outcome.new(balance: balance, transaction_type: :current, purchase_date: Time.zone.today, quotas: 12)
+        expect(outcome.valid?).to eq false
+        expect(outcome.errors.full_messages.first).to eq("Quotas must be blank")
+      end
+    end
   end
 end
