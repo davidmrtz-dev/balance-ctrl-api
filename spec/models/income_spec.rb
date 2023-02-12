2236cc26d0fad727b6ddf31d82bac342a465d4ad
David Mtz <davidelimg1@gmail.com>
2023-02-12 16:37:08 -0600

    [WIP]: Moving balance update logic to transaction

--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Income, type: :model do
     it { should_not allow_value(Time.zone.today).for(:purchase_date).on(:create) }
     it { should_not allow_value(12).for(:quotas).on(:create) }
   end
+
+  describe '#generate_payment' do
+    let!(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
+    let!(:balance) { BalanceFactory.create(user: user, current_amount: 10_000) }
+
+    it "should create one payment for transaction_type 'current'" do
+      expect { Income.create(balance: balance, transaction_type: :current, amount: 5_000, frequency: :monthly) }
+        .to change { Payment.count }.by(1)
+    end
+  end
 end
