71bb5922c589d2bd35409718cc54c212fcb2222f
David Mtz <davidelimg1@gmail.com>
2023-02-12 14:20:58 -0600

    [CHORE]: Updated payments to use factories

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Payment, type: :model do
     describe 'one_payment_for_current_outcome' do
       describe "when paymentable is Income and is 'fixed'" do
         it 'should not be valid if payments > 0' do
-          payment_01 = Payment.create(paymentable: income)
+          payment_01 = PaymentFactory.create(paymentable: income)
           payment_02 = Payment.new(paymentable: income)
 
           expect(payment_02.valid?).to be_falsey
@@ -26,7 +26,7 @@ RSpec.describe Payment, type: :model do
 
       describe "when paymentable is Outcome and is 'fixed'" do
         it 'should not be valid if payments > 0' do
-          payment_01 = Payment.create(paymentable: outcome)
+          payment_01 = PaymentFactory.create(paymentable: outcome)
           payment_02 = Payment.new(paymentable: outcome)
 
           expect(payment_02.valid?).to be_falsey
@@ -43,7 +43,7 @@ RSpec.describe Payment, type: :model do
 
     describe "when paymentable is Outcome and is 'fixed'" do
       it 'should substract the amount from balance current_amount' do
-        payment = Payment.create(paymentable: outcome, amount: 5_000)
+        payment = PaymentFactory.create(paymentable: outcome, amount: 5_000)
 
         expect(balance.reload.current_amount).to eq 5_000
       end
@@ -51,7 +51,7 @@ RSpec.describe Payment, type: :model do
 
     describe "when paymentable is Income is 'fixed'" do
       it 'should add the amount to balance current_amount' do
-        payment = Payment.create(paymentable: income, amount: 5_000)
+        payment = PaymentFactory.create(paymentable: income, amount: 5_000)
 
         expect(balance.reload.current_amount).to eq 15_000
       end
