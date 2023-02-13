5870ba1e151b9348f02d05cdab0a364bdbffb1aa
David Mtz <davidelimg1@gmail.com>
2023-02-12 22:15:32 -0600

    [FEAT]: Added scopes with joins for enhancement

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -4,6 +4,9 @@ class Balance < ApplicationRecord
   has_many :incomes, dependent: :destroy
   has_many :outcomes, dependent: :destroy
 
+  scope :with_outcomes, -> { joins(:outcomes) }
+  scope :current_outcomes, -> { where({ outcomes: { transaction_type: :current } }) }
+
   private
 
   def total_incomes
