6ac9f3afdd06a69655f245be07c0185071c84e25
David Mtz <davidelimg1@gmail.com>
2023-02-12 20:16:31 -0600

    [FEAT]: Implemented multiple payment creation for installment purchases

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -5,11 +5,20 @@ class Outcome < Transaction
   validates :quotas, presence: true, if: -> { transaction_type.eql?('fixed') }
 
   after_create :update_balance_amount, if: -> { transaction_type.eql?('current') }
+  after_create :generate_payments, if: -> { transaction_type.eql?('fixed') }
 
   private
 
   def update_balance_amount
     balance.current_amount -= amount
     balance.save
+  end
+
+  def generate_payments
+    amount_for_quota = amount / quotas
+
+    quotas.times do
+      payments.create!(amount: amount_for_quota)
+    end
   end
 end
