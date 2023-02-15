cff1495c5b321d9ad2f9a613908f61803bade644
David Mtz <davidelimg1@gmail.com>
2023-02-15 10:33:08 -0600

    [CHORE]: Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ balance = Balance.create!(
 10.times do
   Income.create!(
     balance: balance,
-    description: Faker::Lorem.sentence(word_count: 4),
+    description: Faker::Commerce.department(max: 1, fixed_amount: true),
     frequency: :monthly,
     amount: Faker::Number.decimal(l_digits: 4, r_digits: 2)
   )
@@ -26,7 +26,7 @@ end
 40.times do
   Outcome.create!(
     balance: balance,
-    description: Faker::Lorem.sentence(word_count: 4),
+    description: Faker::Commerce.department(max: 1, fixed_amount: true),
     purchase_date: [
       Time.zone.now - 2.days, Time.zone.now - 1.day, Time.zone.now
     ].sample,
