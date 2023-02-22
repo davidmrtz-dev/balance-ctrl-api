00150565cfb72b7e5451e5f51b83304951dea106
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-22 16:58:52 -0600

    [CHORE]: Autoload lib directory

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module BalanceCtrlEnhancedApi
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
+    config.autoloader = :classic
+    config.eager_load_paths << Rails.root.join('lib')
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+      g.request_specs false
+      g.controller_specs true
+      g.assets false
+      g.helper false
+      g.view false
+    end
   end
 end
