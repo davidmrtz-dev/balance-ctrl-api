bbaccb55135eee5332dbba8b4f9471b9cba63eae
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-24 22:03:39 -0600

    [ENHANCE]: Moved general scopes to transactions, implemented tests

--- app/controllers/api/incomes_controller.rb
+++ app/controllers/api/incomes_controller.rb
@@ -3,5 +3,17 @@ module Api
     include Pagination
 
     before_action :authenticate_user!
+
+    def index
+      incomes = Income.
+        with_balance_and_user.
+          from_user(current_user)
+
+      page = paginate(
+        incomes,
+        limit: params[:limit],
+        offset: params[:offset]
+      )
+    end
   end
 end
--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -5,12 +5,8 @@ class Outcome < Transaction
   validates :quotas, presence: true, if: -> { transaction_type.eql?('fixed') }
   validate :purchase_date_not_after_today, on: :create
 
-  scope :with_balance_and_user, -> { joins(balance: :user) }
   scope :current_types, -> { where(transaction_type: :current) }
   scope :fixed_types, -> { where(transaction_type: :fixed) }
-  scope :from_user, lambda { |user|
-    where({ balance: { user: user }})
-  }
   scope :by_purchase_date, -> { order(purchase_date: :desc, id: :desc) }
 
   after_create :substract_balance_amount, if: -> { transaction_type.eql?('current') }
