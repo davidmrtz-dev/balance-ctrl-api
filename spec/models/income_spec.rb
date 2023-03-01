10db718076d53516d6912b7466a05f4a5db0bba6
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-28 22:09:21 -0600

    [WIP]: Granular model test

--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Income, type: :model do
   end
 
   describe 'validations' do
-    it { should allow_value(:monthly).for(:frequency).on(:create) }
     it { should_not allow_value(Time.zone.now).for(:purchase_date).on(:create) }
     it { should_not allow_value(12).for(:quotas).on(:create) }
     it { is_expected.to validate_numericality_of(:amount) }
