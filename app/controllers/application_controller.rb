7e78904404c140559355945b22e92dbeb1281d47
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-08 13:36:23 -0600

    [CHORE]: Updated some configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  include DeviseTokenAuth::Concerns::SetUserByToken
 end
