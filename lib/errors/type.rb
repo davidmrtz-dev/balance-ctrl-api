07ced45f1c169882f7d60f498a83632f94336b78
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-25 18:49:52 -0600

    [FEAT]: Implemented type class for exception capturing

---
+++ lib/errors/type.rb
@@ -0,0 +1,24 @@
+module Errors
+  extend ActiveSupport::Concern
+
+  class Type
+    class << self
+      def capture_exception(exception)
+        case exception
+        when ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed
+          yield I18n.t('errors.invalid_data'), :unprocessable_entity
+        when ActionController::ParameterMissing
+          yield I18n.t('errors.parameter_missing'), :unprocessable_entity
+        when ActiveRecord::RecordNotFound
+          yield I18n.t('errors.record_not_found'), :not_found
+        when ActiveRecord::RecordNotUnique
+          yield I18n.t('errors.invalid_data'), :conflict
+        when Errors::InvalidParameters
+          yield exception.message, :unprocessable_entity
+        else
+          yield I18n.t('errors.internal_server_error'), :internal_server_error
+        end
+      end
+    end
+  end
+end
