2e6ea6d945fee027a63d9eb43679ab03fcaafe0c
davidmrtz-dev <davidelimg1@gmail.com>
2023-09-14 22:38:02 -0600

    [CHORE]: Updated auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/auth'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
