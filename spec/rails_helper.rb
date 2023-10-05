ef3a53024abd2b07fabb7f29ffe5cdffa27e7c6e
davidmrtz-dev <davidelimg1@gmail.com>
2023-10-04 23:54:01 -0600

    [CHORE]: Updated CI test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../config/environments', __dir__)
+# require File.expand_path('../config/environments', __dir__)
 # Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
