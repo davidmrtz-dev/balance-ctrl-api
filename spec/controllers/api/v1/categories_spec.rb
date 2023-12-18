845c3eaa5240da7e564269775172e47ef16a0477
davidmrtz-dev <davidelimg1@gmail.com>
2023-12-17 22:45:56 -0600

    [FEAT]: Categories creation

--- spec/controllers/api/v1/categories_spec.rb
+++ spec/controllers/api/v1/categories_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe Api::V1::CategoriesController, type: :controller do
       expect(parsed_response[:categories].pluck(:id)).to match_array(Category.ids)
     end
   end
+
+  describe 'POST /api/v1/categories' do
+    let(:valid_params) do
+      {
+        category: {
+          name: 'Food'
+        }
+      }
+    end
+
+    subject(:create_category) { post :create, params: valid_params }
+
+    login_user
+
+    it 'creates a category' do
+      expect { create_category }.to change { Category.count }.by(1)
+
+      category = Category.last
+
+      expect(response).to have_http_status(:created)
+      expect(parsed_response[:category][:id]).to eq category.id
+      expect(category.name).to eq 'Food'
+    end
+  end
 end
