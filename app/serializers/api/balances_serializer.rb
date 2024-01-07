b679e2214d52d75c617ab0552354878dada1ed36
davidmrtz-dev <davidelimg1@gmail.com>
2024-01-06 20:26:10 -0600

    [FEAT]: Implemented endpoint to retrieve balances data

---
+++ app/serializers/api/balances_serializer.rb
@@ -0,0 +1,30 @@
+module Api
+  class BalancesSerializer
+    def initialize(balances)
+      @balances = balances
+    end
+
+    def self.json(balances)
+      new(balances).json
+    end
+
+    def json
+      @balances.map do |balance|
+        balance.serializable_hash(
+          methods: %i[
+            amount_incomes
+            amount_outcomes
+            amount_paid
+            amount_to_be_paid
+            amount_for_payments
+          ],
+          except: %i[
+            user_id
+            created_at
+            updated_at
+          ]
+        )
+      end
+    end
+  end
+end
--- spec/controllers/api/v1/balances_spec.rb
+++ spec/controllers/api/v1/balances_spec.rb
@@ -4,6 +4,30 @@ RSpec.describe Api::V1::BalancesController, type: :controller do
   let(:user) { UserFactory.create(email: 'user@example.com', password: 'password') }
   let!(:balance) { BalanceFactory.create_with_attachments(user: user) }
 
+  describe 'GET /api/v1/balances' do
+    login_user
+
+    it 'returns balances data' do
+      get :index
+
+      expect(response).to have_http_status(:ok)
+      expect(parsed_response)
+        .to match({ balances: Api::BalancesSerializer.json(user.balances) }.as_json)
+    end
+  end
+
+  describe 'GET /api/v1/balances/:id' do
+    login_user
+
+    it 'returns balance data' do
+      get :show, params: { id: balance.id }
+
+      expect(response).to have_http_status(:ok)
+      expect(parsed_response)
+        .to match({ balance: Api::BalanceSerializer.json(balance.reload) }.as_json)
+    end
+  end
+
   describe 'GET /api/v1/balances/balance' do
     login_user
 
