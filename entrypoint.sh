516eadbe4c9e1d569a415bb4e85b2c2669650dd7
davidmrtz-dev <davidelimg1@gmail.com>
2024-10-29 20:54:43 -0600

    [FEAT]: Docker working [WIP]: Ports. [FEAT]: Dockerized app [FEAT]: Docker updated, creating db process added [FEAT]: Enhancement

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
+/usr/local/bin/wait-for-it.sh postgres_db:5432 --timeout=60 --strict -- echo "PostgreSQL is up"
+
+bundle exec rake db:create || true
+
 bundle exec rake db:migrate
 
 exec bundle exec "$@"
