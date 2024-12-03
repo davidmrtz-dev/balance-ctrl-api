dc2a72e2b68ec054b543851dc0331ad5b99d7d79
davidmrtz-dev <davidelimg1@gmail.com>
2024-12-03 10:58:17 -0600

    [FEAT]: Added webhook simple logic

--- app/controllers/api/v1/balances_controller.rb
+++ app/controllers/api/v1/balances_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class BalancesController < ApiController
-      before_action :authenticate_user!
+      before_action :authenticate_user!, except: :webhook
 
       def index
         balances = current_user.balances.first(12)
@@ -19,6 +19,14 @@ module Api
         balance = current_user.current_balance
 
         render json: { balance: ::Api::BalanceSerializer.json(balance) }
+      end
+
+      def webhook
+        Rails.logger.info "Webhook received: #{params.inspect}"
+
+        render json: { message: 'Webhook received', params: params }
+
+        # render json: { errors: 'Unauthorized' }, status: :unauthorized
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post 'webhooks/notifications/email', to: 'balances#webhook'
       get 'balance', to: 'balances#balance'
       resources :outcomes, only: %i[index create update destroy] do
         collection do
